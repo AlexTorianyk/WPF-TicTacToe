1aadbbd114b41ef8fe46590c0eab9c584cc36066
AlexToryanik <alex1toryanik@gmail.com>
2018-07-13 12:42:22 +0300

    Used OnPropertyChanged() in the PlayerModel class.

--- TicTacToe/Model/PlayerModel.cs
+++ TicTacToe/Model/PlayerModel.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicTacToe.Utilities;
 
 namespace TicTacToe.Model
 {
-    public class PlayerModel
+    public class PlayerModel : PropertyChange
     {
         private string _playerColor;
         private string _playerSymbol;
         private int _playerScore = 0;
 
-
+        public string PlayerColor { get => _playerColor; set => _playerColor = value; }
+        public string PlayerSymbol { get => _playerSymbol; set => _playerSymbol = value; }
+        public int PlayerScore
+        {
+            get => _playerScore;
+            set { _playerScore = value; OnPropertyChanged(); }
+        }
     }
 }
