6c93bde8290beb7a3ed4b3bff03552004c771953
AlexToryanik <alex1toryanik@gmail.com>
2018-07-17 12:40:25 +0300

    Implemented the checking for winning and drawing logic in GameBoardModel.

--- TicTacToe/Model/GameBoardModel.cs
+++ TicTacToe/Model/GameBoardModel.cs
@@ -25,16 +25,72 @@ namespace TicTacToe.Model
 
         private void ResetGrid()
         {
-            GameGrid = new ObservableCollection<char> {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'};
+            GameGrid = new ObservableCollection<char> {'E', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         }
 
         // Simulates a player placing a symbol on the grid
         public void TakeTurn(int placement, char symbol)
         {
-            if (GameGrid[placement] == 'E')
+            if (GameGrid[placement] == ' ')
             {
                 GameGrid[placement] = symbol;
             }
         }
+
+        public bool CheckWin()
+        {
+            bool ViVon = false;
+            return ViVon;
+        }
+
+        #region CheckWin Helpers
+        private bool CheckHorizontalWin()
+        {
+            if (GameGrid[1] != ' ' && ((GameGrid[1] == GameGrid[2]) && (GameGrid[2] == GameGrid[3])))
+                return true;
+            else if (GameGrid[4] != ' ' && ((GameGrid[4] == GameGrid[5]) && (GameGrid[5] == GameGrid[6])))
+                return true;
+            else if (GameGrid[7] != ' ' && ((GameGrid[7] == GameGrid[8]) && (GameGrid[8] == GameGrid[9])))
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckVerticalWin()
+        {
+            if (GameGrid[1] != ' ' && ((GameGrid[1] == GameGrid[4]) && (GameGrid[4] == GameGrid[7])))
+                return true;
+            else if (GameGrid[2] != ' ' && ((GameGrid[2] == GameGrid[5]) && (GameGrid[5] == GameGrid[8])))
+                return true;
+            else if (GameGrid[3] != ' ' && ((GameGrid[3] == GameGrid[6]) && (GameGrid[6] == GameGrid[9])))
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckDiagonalWin()
+        {
+            if (GameGrid[1] != ' ' && ((GameGrid[1] == GameGrid[5]) && (GameGrid[5] == GameGrid[9])))
+                return true;
+            else if (GameGrid[3] != ' ' && ((GameGrid[3] == GameGrid[5]) && (GameGrid[5] == GameGrid[7])))
+                return true;
+            else
+                return false;
+        }
+        #endregion
+
+        //if atleast on of the tiles is blank, no draw
+        public bool CheckDraw()
+        {
+            foreach (char tile in GameGrid)
+            {
+                if (tile == ' ')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
     }
 }
