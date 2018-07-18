8114cffc47c708a08e0e8fb95f7b6634f89603c5
AlexToryanik <alex1toryanik@gmail.com>
2018-07-18 12:48:37 +0300

    Added the GameOverToHitTestVisibleConverter.

---
+++ TicTacToe/Utilities/ValueConverters/GameOverToHitTestVisibleConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TicTacToe.Utilities.ValueConverters
+{
+    public class GameOverToHitTestVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                switch (value)
+                {
+                    case false:
+                        return "True";
+                    case true:
+                        return "False";
+                    default:
+                        return "True";
+                }
+            }
+            return "True";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
