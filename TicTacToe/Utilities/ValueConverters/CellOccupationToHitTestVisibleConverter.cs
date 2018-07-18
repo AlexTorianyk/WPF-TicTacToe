96f16e5a5954f56dd305ad783d04a58be1caf7b7
AlexToryanik <alex1toryanik@gmail.com>
2018-07-18 12:43:27 +0300

    Changed the naming of on of the CellOccupationToEnabled converter to CellOccupationToHitTestVisibleConverter.

--- TicTacToe/Utilities/ValueConverters/CellOccupationToHitTestVisibleConverter.cs
+++ TicTacToe/Utilities/ValueConverters/CellOccupationToHitTestVisibleConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace TicTacToe.Utilities.ValueConverters
 {
-    public class CellOccupationToEnabledConverter : IValueConverter
+    public class CellOccupationToHitTestVisibleConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
