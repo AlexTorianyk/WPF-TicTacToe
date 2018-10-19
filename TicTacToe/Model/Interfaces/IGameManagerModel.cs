56b957843b01b27b3f960f0263aef89043e44e32
aleks.toryanik <aleks.toryanik@mail.ru>
2018-10-19 18:51:52 +0200

    Removed unnecessary things from IGameManagerModel.

--- TicTacToe/Model/Interfaces/IGameManagerModel.cs
+++ TicTacToe/Model/Interfaces/IGameManagerModel.cs
@@ -2,9 +2,6 @@
 {
     public interface IGameManagerModel
     {
-        bool GameOver { get; set; }
-        IPlayerModel Player1 { get; set; }
-        IPlayerModel Player2 { get; set; }
         IPlayerModel CurrentPlayer { get; set; }
         void ResetGameData();
         IGameBoardModel GameBoard { get; set; }
